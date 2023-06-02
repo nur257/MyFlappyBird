c7c82f2c68c11344cba152082a4211e7f1af2ae2
nur257 <135322967+nur257@users.noreply.github.com>
2023-06-02 23:28:45 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1685715230,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"media/enemyhit.m4a",
+		"media/enemyhit.ogg",
+		"media/mattoglseby - 1.m4a",
+		"media/flappy bird flap sound.m4a",
+		"media/jump.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
